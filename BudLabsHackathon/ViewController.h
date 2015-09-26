51ca92990186f950b5f2c4948c435512fd217362
ssaini4 <ssaini4@illinois.edu>
2015-09-25 22:36:50 -0500

    Initial Commit

---
+++ BudLabsHackathon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BudLabsHackathon
+//
+//  Created by Saksham Saini on 9/25/15.
+//  Copyright © 2015 Saksham Saini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BudLabsHackathon/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BudLabsHackathon
+//
+//  Created by Saksham Saini on 9/25/15.
+//  Copyright © 2015 Saksham Saini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
