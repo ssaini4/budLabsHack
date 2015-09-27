012d4323f0e102cd7fd6087ba261c2582825ab21
ssaini4 <ssaini4@illinois.edu>
2015-09-26 19:42:50 -0500

    Working Copy

---
+++ BudLabsHackathon/QRCodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  QRCodeViewController.h
+//  BudLabsHackathon
+//
+//  Created by Saksham Saini on 9/26/15.
+//  Copyright © 2015 Saksham Saini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QRCodeViewController : UITableViewController
+
+@end
